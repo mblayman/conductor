892cc1afe6127f2bbea8a48d854675226fb5edb1
Matt Layman <matthewlayman@gmail.com>
2016-01-07 22:21:29 -0500

    Use a different settings file for tests.

--- manage.py
+++ manage.py
@@ -2,8 +2,11 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lcp.settings")
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lcp.settings.development')
+
+    if 'test' in sys.argv:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'lcp.settings.test'
 
     from django.core.management import execute_from_command_line
 
