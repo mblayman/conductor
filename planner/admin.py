bf600b11121db8801811256edc4649b66d5c67a8
Matt Layman <matthewlayman@gmail.com>
2016-01-11 01:33:25 -0500

    Create a basic school admin.

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from planner.models import School
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+    search_fields = ['name']
--- planner/models.py
+++ planner/models.py
@@ -8,5 +8,8 @@ class Milestone(models.Model):
 class School(models.Model):
     name = models.TextField()
     slug = models.SlugField(max_length=256, unique=True)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     milestones_url = models.URLField()
+
+    def __str__(self):
+        return self.name
