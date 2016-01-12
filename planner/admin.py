854179776bfdd01cd98ea2fa7de963e225e0ce31
Matt Layman <matthewlayman@gmail.com>
2016-01-11 21:36:33 -0500

    Make the admin more useful for viewing school data.

--- planner/admin.py
+++ planner/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from planner.models import School
 
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url')
+    def clickable_url(obj):
+        return format_html(
+            '<a href="{0}" target="_blank">{1}</a>', obj.url, obj.url)
+    clickable_url.short_description = 'url'
+
+    list_display = ('name', clickable_url, 'milestones_url')
+    ordering = ['id']
     search_fields = ['name']
