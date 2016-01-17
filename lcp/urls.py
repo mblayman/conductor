f9d4ec08a2e88d52fb02949d00c3ad25fd81d1df
Matt Layman <matthewlayman@gmail.com>
2016-01-16 21:51:49 -0500

    Wire the school views into the URL configuration.

--- lcp/urls.py
+++ lcp/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework import routers
+
+from planner.views import SchoolViewSet
+
+router = routers.DefaultRouter()
+router.register('schools', SchoolViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
