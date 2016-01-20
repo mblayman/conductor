b08d3d43bd485f64da107eb2bc129d44f6ac7540
Matt Layman <matthewlayman@gmail.com>
2016-01-20 00:04:38 -0500

    Work around an Ember Data bug.

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -3,4 +3,8 @@ import {
   setResolver
 } from 'ember-qunit';
 
+// XXX: This is what I get for being on the bleeding edge. Bugs.
+// See emberjs/data#4071.
+import "ember-data";
+
 setResolver(resolver);
