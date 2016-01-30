ebbf8f93488ee46196edd8bb92739f6553e1e0b4
Matt Layman <matthewlayman@gmail.com>
2016-01-30 08:56:08 -0500

    Merge `ember init` results.

--- client/tests/test-helper.js
+++ client/tests/test-helper.js
@@ -3,8 +3,4 @@ import {
   setResolver
 } from 'ember-qunit';
 
-// XXX: This is what I get for being on the bleeding edge. Bugs.
-// See emberjs/data#4071.
-import "ember-data";
-
 setResolver(resolver);
