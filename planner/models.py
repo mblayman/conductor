341c0126c77cda3c60d583a7c8632a1297fea53c
Matt Layman <matthewlayman@gmail.com>
2016-02-03 00:34:16 -0500

    Display unicode characters.

--- planner/models.py
+++ planner/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
 class Milestone(models.Model):
     date = models.DateTimeField()
 
 
+@python_2_unicode_compatible
 class School(models.Model):
     name = models.TextField()
     slug = models.SlugField(max_length=256, unique=True)
