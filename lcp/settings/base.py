01d49c43594829c5ba10c4dcb6db00bfd5e5fb61
Matt Layman <matthewlayman@gmail.com>
2016-02-07 21:03:17 -0500

    Make the default for DEBUG be False.

--- lcp/settings/base.py
+++ lcp/settings/base.py
@@ -4,3 +4,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ['SECRET_KEY']
+
+# Other settings must explicitly opt-in for debug mode.
+DEBUG = False
