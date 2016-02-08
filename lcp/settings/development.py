abb240314024e6c81683ee12b6523f967947a424
Matt Layman <matthewlayman@gmail.com>
2016-02-07 19:02:43 -0500

    Start a base settings file.

--- lcp/settings/development.py
+++ lcp/settings/development.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from lcp.settings.base import *  # noqa
 
 
 # Quick-start development settings - unsuitable for production
