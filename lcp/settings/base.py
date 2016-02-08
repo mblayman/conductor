bf2d45dc75f3ce200573a77ec69209fbc46209e8
Matt Layman <matthewlayman@gmail.com>
2016-02-07 21:16:07 -0500

    Move TEMPLATES.

--- lcp/settings/base.py
+++ lcp/settings/base.py
@@ -35,3 +35,19 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'lcp.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
