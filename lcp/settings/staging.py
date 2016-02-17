8d167a4db654dab46a0afbdd620349db9c68dc82
Matt Layman <matthewlayman@gmail.com>
2016-02-16 23:21:47 -0500

    Set temporary ALLOWED_HOSTS for Vagrant testing.

--- lcp/settings/staging.py
+++ lcp/settings/staging.py
@@ -2,6 +2,10 @@ import os
 
 from lcp.settings.base import *  # noqa
 
+# FIXME: The wildcard is only here while testing on Vagrant.
+# Host header checking fails without it.
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
