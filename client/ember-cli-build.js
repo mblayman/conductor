aa6db5d1ff6a940c3926c3e9604152b50539dd16
Matt Layman <matthewlayman@gmail.com>
2016-04-02 15:24:42 -0400

    Add the fonts to the assets output.

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -1,6 +1,7 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var Funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -23,6 +24,11 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  var semanticFonts = new Funnel(
+    'node_modules/semantic-ui-less/themes/default/assets/fonts', {
+      srcDir: '/',
+      destDir: '/assets/fonts'
+    });
 
-  return app.toTree();
+  return app.toTree(semanticFonts);
 };
