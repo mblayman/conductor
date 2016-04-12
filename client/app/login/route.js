30af243b156565a251b74fffcd290374de4aeb31
Matt Layman <matthewlayman@gmail.com>
2016-04-11 22:39:12 -0400

    Create a user record for validation purposes.
    The test is missing because I'm having a very hard time
    running a unit test that will return a user instance.
    For reasons unknown to me, the returned instance in the test
    looks like "Class".

--- client/app/login/route.js
+++ client/app/login/route.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  model() {
+    return this.store.createRecord('user');
+  }
+});
