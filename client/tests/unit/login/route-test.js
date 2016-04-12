5cf83322d8c9d67c1db672d2d3e30e21257f5786
Matt Layman <matthewlayman@gmail.com>
2016-04-12 08:27:50 -0400

    Add a login model hook test.
    The problem I had last night was that modelName is not
    available on a model instance.

--- client/tests/unit/login/route-test.js
+++ client/tests/unit/login/route-test.js
@@ -1,11 +1,19 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:login', 'Unit | Route | login', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['model:user']
 });
 
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+});
+
+test('has a user for its model', function(assert) {
+  let route = this.subject();
+  Ember.run(function() {
+    let user = route.model();
+    assert.equal(user.get('constructor.modelName'), 'user');
+  });
 });
