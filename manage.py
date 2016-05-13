272ece1774cebaf8d6d6ae9e0dfb5fe0cce97083
Matt Layman <matthewlayman@gmail.com>
2016-05-12 20:55:01 -0400

    Add missing env variables for testing.

--- manage.py
+++ manage.py
@@ -8,7 +8,9 @@ if __name__ == '__main__':
     if 'test' in sys.argv:
         # For now, fake setting the environment for testing.
         os.environ['DJANGO_SETTINGS_MODULE'] = 'conductor.settings.test'
+        os.environ['CORS_ORIGIN_WHITELIST'] = 'localhost:4200'
         os.environ['SECRET_KEY'] = 'asecrettoeverybody'
+        os.environ['STATIC_URL'] = '/static/'
 
     from django.core.management import execute_from_command_line
 
