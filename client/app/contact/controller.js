2cc45214f4b3200d613cf7996b70f9484e237e19
Matt Layman <matthewlayman@gmail.com>
2016-05-17 22:16:11 -0400

    Submit support tickets to API.
    Fixes #64

--- client/app/contact/controller.js
+++ client/app/contact/controller.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     submit() {
+      console.log(this.get('model').get('email'));
+      console.log(this.get('model').get('subject'));
+      console.log(this.get('model').get('message'));
+      this.get('model').save();
     }
   }
 });
