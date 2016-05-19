6224887fdccc77b6198c7e8e5ea90da5dbe7aa68
Matt Layman <matthewlayman@gmail.com>
2016-05-18 23:18:31 -0400

    Add validation for the contact form.
    Fixes #66

--- client/app/contact/controller.js
+++ client/app/contact/controller.js
@@ -1,12 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  didValidate: false,
+
   actions: {
     submit() {
-      console.log(this.get('model').get('email'));
-      console.log(this.get('model').get('subject'));
-      console.log(this.get('model').get('message'));
-      this.get('model').save();
+      this.get('model').validate().then(({model, validations}) => {
+        if (validations.get('isValid')) {
+          this.onValid(model);
+        }
+        this.set('didValidate', true);
+      });
     }
+  },
+
+  onValid(model) {
+    console.log(model.get('email'));
+    console.log(model.get('subject'));
+    console.log(model.get('message'));
+    // this.get('model').save();
   }
 });
