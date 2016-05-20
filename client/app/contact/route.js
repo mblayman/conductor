fe33367fdedf5c6b4b4b9fc672be48c49a1c5192
Matt Layman <matthewlayman@gmail.com>
2016-05-20 00:12:08 -0400

    Reset the contact form validation between transitions.
    Fixes #69

--- client/app/contact/route.js
+++ client/app/contact/route.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.createRecord('support-ticket');
+  },
+
+  // Controllers are singletons so state must be cleared.
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.set('didValidate', false);
   }
 });
