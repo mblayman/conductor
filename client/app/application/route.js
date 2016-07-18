8c41f0583279d41a1d65be5edda5dd5ea7b79662
Matt Layman <matthewlayman@gmail.com>
2016-07-17 22:08:42 -0400

    Fix the title to be in the right order.

--- client/app/application/route.js
+++ client/app/application/route.js
@@ -3,6 +3,7 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   title(tokens) {
+    tokens.reverse();
     tokens.push('College Conductor');
     return tokens.join(' - ');
   }
