ec8a5892be0b905f6ff66d0dfb72d0b9c5251424
Matt Layman <matthewlayman@gmail.com>
2016-08-21 23:07:59 -0400

    Handle useless student records upon leaving the form route.
    Fixes #88

--- client/app/dashboard/students/new/route.js
+++ client/app/dashboard/students/new/route.js
@@ -7,6 +7,14 @@ export default Ember.Route.extend({
     return this.store.createRecord('student');
   },
 
+  actions: {
+    willTransition() {
+      // Clean out any unsaved students from the store.
+      const model = this.modelFor(this.routeName);
+      model.rollbackAttributes();
+    }
+  },
+
   resetController(controller, isExiting) {
     if (isExiting) {
       controller.set('didValidate', false);
