9169486d64328fcae0fe817572d69ab942b796b9
Matt Layman <matthewlayman@gmail.com>
2016-08-27 21:57:59 -0400

    Upgrade ember to 2.8.0-beta.3.

--- client/config/environment.js
+++ client/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'client',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
 
     'ember-simple-auth': {
@@ -42,7 +42,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
