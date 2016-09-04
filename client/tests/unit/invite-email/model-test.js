40b0154a094268e28d9b30246e46ab52a94938ac
Matt Layman <matthewlayman@gmail.com>
2016-09-03 21:00:15 -0400

    Add InviteEmail as an Ember model.
    Fixes #96

---
+++ client/tests/unit/invite-email/model-test.js
@@ -0,0 +1,17 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('invite-email', 'Unit | Model | invite email', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.ok(!!model);
+});
+
+test('has an email', function(assert) {
+  let model = this.subject();
+  assert.ok(model.toJSON().hasOwnProperty('email'));
+});
