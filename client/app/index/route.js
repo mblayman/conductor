68e0d508b819bc7fc32783821ad0bae56139d4be
Matt Layman <matthewlayman@gmail.com>
2016-09-05 15:52:14 -0400

    Clean up the index route on transition.
    Fixes #101

--- client/app/index/route.js
+++ client/app/index/route.js
@@ -5,6 +5,21 @@ export default Ember.Route.extend({
     return this.store.createRecord('invite-email');
   },
 
+  actions: {
+    willTransition() {
+      // Clean out any unsaved emails from the store.
+      const model = this.modelFor(this.routeName);
+      model.rollbackAttributes();
+    }
+  },
+
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.set('didValidate', false);
+      controller.set('errorMessage', false);
+    }
+  },
+
   renderTemplate() {
     this.render();
     this.render('index-masthead', {
