d870753d5e4c55ecb598a730a747cd608659cdc7
Matt Layman <matthewlayman@gmail.com>
2016-09-05 15:43:05 -0400

    Add the model hook for the invite email on the index route.
    Fixes #100

--- client/app/index/route.js
+++ client/app/index/route.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.createRecord('invite-email');
+  },
+
   renderTemplate() {
     this.render();
     this.render('index-masthead', {
--- client/tests/unit/index/route-test.js
+++ client/tests/unit/index/route-test.js
@@ -1,11 +1,24 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:index', 'Unit | Route | index', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: [
+    'model:invite-email',
+    'validator:format',
+    'validator:presence'
+  ]
 });
 
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+});
+
+test('it has an invite email for its model', function(assert) {
+  let route = this.subject();
+  Ember.run(function() {
+    let inviteEmail = route.model();
+    assert.equal(inviteEmail.get('constructor.modelName'), 'invite-email');
+  });
 });
