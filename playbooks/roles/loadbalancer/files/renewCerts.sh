e8b15f4f3a8497dbfcbc200ba5afa40d4a7a02fd
Matt Layman <matthewlayman@gmail.com>
2016-11-03 23:01:59 -0400

    Add Let's Encrypt renewal script.
    Fixes #120

---
+++ playbooks/roles/loadbalancer/files/renewCerts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script renews all the Let's Encrypt certificates with a validity < 30 days
+
+if ! /usr/bin/letsencrypt renew > /var/log/letsencrypt/renew.log 2>&1 ; then
+    echo "Automated renewal failed:"
+    cat /var/log/letsencrypt/renew.log
+    exit 1
+fi
+service nginx restart
