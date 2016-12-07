0986fbee76028b1aa819ab09fb04323a03736d06
Matt Layman <matthewlayman@gmail.com>
2016-12-06 22:58:19 -0500

    Upgrade to Django 1.10.
    Fixes #93

--- conductor/settings/base.py
+++ conductor/settings/base.py
@@ -28,14 +28,13 @@ INSTALLED_APPS = [
     'vendor.apps.VendorConfig',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
