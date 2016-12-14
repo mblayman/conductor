c21253075a28c29e9a2cb952f7f037429bffffb9
Matt Layman <matthewlayman@gmail.com>
2016-12-13 21:08:14 -0500

    Add a user admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from accounts.models import InviteEmail
+from accounts.models import InviteEmail, User
 
 
 @admin.register(InviteEmail)
 class InviteEmailAdmin(admin.ModelAdmin):
     list_display = ('email', 'created_date')
     search_fields = ['email']
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
