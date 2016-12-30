97ec0e83fca6bc25f5f208e57b2dcf8978f27255
Matt Layman <matthewlayman@gmail.com>
2016-12-29 21:41:22 -0500

    Connect query input to controller and model hook.

--- client/app/dashboard/find-school/route.js
+++ client/app/dashboard/find-school/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    return this.store.query('school', params);
+  }
 });
