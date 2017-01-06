5970c979335066405d873dc97196d0a0161012cd
Matt Layman <matthewlayman@gmail.com>
2017-01-05 22:47:17 -0500

    Add an action that will create a target school.
    Fixes #177

--- client/app/dashboard/student/route.js
+++ client/app/dashboard/student/route.js
@@ -4,5 +4,14 @@ export default Ember.Route.extend({
   // TODO: test this model hook.
   model(params) {
     return this.store.findRecord('student', params.student_id);
+  },
+
+  actions: {
+    createTargetSchool(school) {
+      const student = this.controller.get('model');
+      // TODO: Save a target school.
+      // TODO: Show error modal if saving fails (e.g., unique constraint failure)
+      console.log('From route, creating target school with', school.get('name'), student.get('firstName'));
+    }
   }
 });
