332772894e27853d7d53415fe795e106783615a5
Matt Layman <matthewlayman@gmail.com>
2017-02-20 21:17:34 -0500

    Use a span so that labels don't wrap.

--- client/app/components/milestone-label/component.js
+++ client/app/components/milestone-label/component.js
@@ -9,6 +9,7 @@ const COLORS = {
 };
 
 export default Ember.Component.extend({
+  tagName: 'span',
   color: Ember.computed('milestone.category', function () {
     const category = this.get('milestone.category');
     // When the milestone hasn't resolve yet, return no color.
