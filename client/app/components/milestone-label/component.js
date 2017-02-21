6d7d47af4b7b0d9e60aac35d20df1a3e23bbaf1f
Matt Layman <matthewlayman@gmail.com>
2017-02-20 20:39:12 -0500

    Show milestones!
    Fixes #198

---
+++ client/app/components/milestone-label/component.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+const COLORS = {
+  ED: 'pink',
+  ED1: 'blue',
+  ED2: 'green',
+  EA: 'orange',
+  RD: 'grey'
+};
+
+export default Ember.Component.extend({
+  color: Ember.computed('milestone.category', function () {
+    const category = this.get('milestone.category');
+    // When the milestone hasn't resolve yet, return no color.
+    if (!category) { return ''; }
+    return COLORS[this.get('milestone.category')];
+  })
+});
