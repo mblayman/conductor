8107068eef203ffd533c18eb7754cfbe3898ee02
Matt Layman <matthewlayman@gmail.com>
2017-02-24 20:41:49 -0500

    Trim the fat.

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -19,7 +19,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/semantic/dist/semantic.css');
+  app.import('bower_components/semantic/dist/semantic.min.css');
 
   var semanticFonts = new Funnel(
     'bower_components/semantic/dist/themes/default/assets', {
