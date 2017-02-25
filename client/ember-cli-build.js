b1ae06b8ac696566a21d5acef180fbaf0ae4f769
Matt Layman <matthewlayman@gmail.com>
2017-02-24 21:03:26 -0500

    Integrate Gulp and Semantic UI building into deployment.

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -19,7 +19,7 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('vendor/semantic/dist/semantic.min.css');
+  app.import('vendor/semantic/dist/semantic.css');
 
   var semanticFonts = new Funnel(
     'vendor/semantic/dist/themes/default/assets', {
