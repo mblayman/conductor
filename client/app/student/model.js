79b265a0fbfc5c3dccf183871a15806803524cb4
Matt Layman <matthewlayman@gmail.com>
2017-02-27 21:39:45 -0500

    Clean up the student model with a fullName property.

--- client/app/student/model.js
+++ client/app/student/model.js
@@ -1,12 +1,19 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
 import StudentValidations from 'client/mixins/student-validations';
 
+const { computed } = Ember;
+
 export default Model.extend(StudentValidations, {
   firstName: attr('string'),
   lastName: attr('string'),
   matriculationSemester: belongsTo('semester'),
-  schools: hasMany('school')
+  schools: hasMany('school'),
+
+  fullName: computed('firstName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })
 });
