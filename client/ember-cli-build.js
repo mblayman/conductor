8580f2a69663b50a41ab8e43b336113a5ea41665
Matt Layman <matthewlayman@gmail.com>
2017-06-01 23:29:54 -0400

    Integrate Semantic UI JavaScript into vendor bundle.
    Semantic wasn't working because the code did not initialize plugins in
    didInsertElement. After that was wired up, the fancier stuff started to
    work.
    
    Fixes #237

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -19,6 +19,7 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
   app.import('vendor/semantic/dist/semantic.css');
+  app.import('vendor/semantic/dist/semantic.js');
 
   var semanticFonts = new Funnel(
     'vendor/semantic/dist/themes/default/assets', {
