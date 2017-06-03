6025d8fc36e6dba913aba3778d2530a1f90fa8c9
Matt Layman <matthewlayman@gmail.com>
2017-06-02 21:45:39 -0400

    Make input fields validate and show errors after losing focus.
    This will be a boon to the signup process that needs to do server side
    validation.
    
    Fixes #242

--- client/app/components/field-input/component.js
+++ client/app/components/field-input/component.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
   classNames: ['field'],
-  classNameBindings: ['isInvalid:error']
+  classNameBindings: ['showError:error'],
+  hadFocus: false,
+  formValidated: false,
+  isInvalid: false,
+  showError: computed('hadFocus', 'formValidated', 'isInvalid', function() {
+    // Show the error message when a user has visited a field
+    // or they submit the form and errors exist.
+    return (this.get('hadFocus') || this.get('formValidated')) && this.get('isInvalid');
+  })
 });
