05c6ca23dc305345f9b07b183fbfd58b8ab8752e
Matt Layman <matthewlayman@gmail.com>
2017-06-05 00:18:31 -0400

    Add password to Ember user model.

--- client/app/user/model.js
+++ client/app/user/model.js
@@ -5,4 +5,5 @@ import UserValidations from 'client/mixins/user-validations';
 export default DS.Model.extend(UserValidations, {
   username: DS.attr('string'),
   email: DS.attr('string'),
+  password: DS.attr('string'),
 });
