c52af3dcde289d9ec3fe8bd6ce07b6eec634080e
Matt Layman <matthewlayman@gmail.com>
2017-11-12 14:12:18 -0500

    Get token from Stripe.
    For #269

--- landing/_assets/js/signup.js
+++ landing/_assets/js/signup.js
@@ -1,7 +1,7 @@
-// Create a Stripe client
+// Create a Stripe client.
 var stripe = Stripe(window.stripePublishableKey);
 
-// Create an instance of Elements
+// Create an instance of Elements.
 var elements = stripe.elements();
 
 // Custom styling can be passed to options when creating an Element.
@@ -9,7 +9,6 @@ var elements = stripe.elements();
 var style = {
   base: {
     color: '#32325d',
-    lineHeight: '24px',
     fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
     fontSmoothing: 'antialiased',
     fontSize: '16px',
@@ -23,10 +22,10 @@ var style = {
   }
 };
 
-// Create an instance of the card Element
+// Create an instance of the card Element.
 var card = elements.create('card', {style: style});
 
-// Add an instance of the card Element into the `card-element` <div>
+// Add an instance of the card Element into the `card-element` <div>.
 card.mount('#card-element');
 
 // Handle real-time validation errors from the card Element.
@@ -43,31 +42,32 @@ $('#why-ask').popover({
   trigger: 'focus'
 });
 
+var completeSignup = function(stripeToken) {
+  // Check the validity again in case creating a token took a long time
+  // and the user somehow managed to screw up the form.
+  var form = document.getElementById('signup-form');
+  var isValid = form.checkValidity();
+  console.log(isValid, stripeToken);
+};
+
 window.addEventListener('load', function() {
   var form = document.getElementById('signup-form');
   form.addEventListener('submit', function(event) {
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Check the regular form fields.
+    var isValid = form.checkValidity();
+
+    stripe.createToken(card).then(function(result) {
+      if (result.error) {
+        var errorElement = document.getElementById('card-errors');
+        errorElement.textContent = result.error.message;
+      } else {
+        completeSignup(result.token);
+      }
+    });
+
     form.classList.add('was-validated');
   }, false);
 }, false);
-
-// TODO: review this.
-// // Handle form submission
-// var form = document.getElementById('payment-form');
-// form.addEventListener('submit', function(event) {
-//   event.preventDefault();
-
-//   stripe.createToken(card).then(function(result) {
-//     if (result.error) {
-//       // Inform the user if there was an error
-//       var errorElement = document.getElementById('card-errors');
-//       errorElement.textContent = result.error.message;
-//     } else {
-//       // Send the token to your server
-//       stripeTokenHandler(result.token);
-//     }
-//   });
-// });
