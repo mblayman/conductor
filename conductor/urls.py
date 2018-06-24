7458fa29e8ffa8ae665ceef77415f62b6f67fb75
Matt Layman <matthewlayman@gmail.com>
2018-06-24 17:44:19 -0400

    Upgrade to Django 2.0.
    Fixes #312

--- conductor/urls.py
+++ conductor/urls.py
@@ -1,7 +1,7 @@
 import os
 
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 from accounts.views import dashboard, signup
@@ -10,42 +10,31 @@ from support.views import contact
 
 
 urlpatterns = [
-    url(r'^$',
-        TemplateView.as_view(template_name='index.html'),
-        name='index'),
-    url(r'^signup/$',
-        signup,
-        name='signup'),
-    url(r'^app/$',
-        dashboard,
-        name='dashboard'),
-    url(r'^contact/$',
-        contact,
-        name='contact'),
-    url(r'^terms/$',
-        TemplateView.as_view(template_name='terms.html'),
-        name='terms'),
-    url(r'^privacy/$',
-        TemplateView.as_view(template_name='privacy.html'),
-        name='privacy'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    # Marketing/non-authenticated views
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('signup/', signup, name='signup'),
+    path('contact/', contact, name='contact'),
+    path('terms/',
+         TemplateView.as_view(template_name='terms.html'), name='terms'),
+    path('privacy/',
+         TemplateView.as_view(template_name='privacy.html'), name='privacy'),
 
-    url(r'^students/add/',
-        add_student,
-        name='add-student'),
-    url(r'^students/(?P<student_id>\d+)/$',
-        student_profile,
-        name='student-profile'),
-    url(r'^students/(?P<student_id>\d+)/add-school/',
-        add_school,
-        name='add-school'),
+    path('app/', dashboard, name='dashboard'),
 
-    url(r'^admin/', admin.site.urls),
+    # Students
+    path('students/add/', add_student, name='add-student'),
+    path('students/<int:student_id>/',
+         student_profile, name='student-profile'),
+    path('students/<int:student_id>/add-school/',
+         add_school, name='add-school'),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 if os.environ['DJANGO_SETTINGS_MODULE'] == 'conductor.settings.development':
     import debug_toolbar
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += staticfiles_urlpatterns()
