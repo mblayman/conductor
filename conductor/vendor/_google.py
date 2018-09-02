7819d5dec2373bdc95c0383dc02cf0b032361249
Matt Layman <matthewlayman@gmail.com>
2018-09-01 22:53:26 -0400

    Output the header row.
    For #288

--- conductor/vendor/_google.py
+++ conductor/vendor/_google.py
@@ -1,7 +1,26 @@
+from typing import List
+
 from apiclient import discovery
 from google.oauth2.credentials import Credentials
 
 from conductor.planner.models import Student
+
+SCHEDULE_HEADER_ROW = [
+    "School",
+    "Application Platform",
+    "Plan",
+    "Out Due Date",
+    "Official Due Date",
+    "App Form Status",
+    "Essay List (words)",
+    "Essay Status",
+    "Recs (req/allowed)",
+    "Rec Status",
+    "Transcript/School Report",
+    "Transcript Status",
+    "Test Scores",
+    "Test Scores Status",
+]
 
 
 class GoogleGateway:
@@ -18,5 +37,30 @@ class GoogleGateway:
         sheets_client = discovery.build(
             "sheets", "v4", credentials=self.credentials, cache_discovery=False
         )
-        data = {"properties": {"title": "{} - Application Status".format(student)}}
+        data = {
+            "properties": {"title": "{} - Application Status".format(student)},
+            "sheets": [
+                {
+                    "properties": {
+                        "gridProperties": {"frozenRowCount": 1, "frozenColumnCount": 1}
+                    },
+                    "data": [
+                        {
+                            "startRow": 0,
+                            "startColumn": 0,
+                            "rowData": [self.build_header_row()],
+                        }
+                    ],
+                }
+            ],
+        }
         sheets_client.spreadsheets().create(body=data).execute()
+
+    def build_header_row(self) -> List:
+        """Build the header row of cells."""
+        return {
+            "values": [
+                {"userEnteredValue": {"stringValue": column_name}}
+                for column_name in SCHEDULE_HEADER_ROW
+            ]
+        }
--- conductor/vendor/tests/test_google.py
+++ conductor/vendor/tests/test_google.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from unittest import mock
 
 from conductor.tests import TestCase
@@ -6,7 +7,7 @@ from conductor.vendor._google import GoogleGateway
 
 @mock.patch("conductor.vendor._google.discovery")
 class TestGoogleGateway(TestCase):
-    def setup_mock_for_sheets(self, mock_discovery):
+    def setup_mock_for_sheets(self, mock_discovery: mock.MagicMock) -> None:
         """Set up the mocks to pretend to be the discovery API.
 
         This is a maze.
@@ -16,11 +17,11 @@ class TestGoogleGateway(TestCase):
         sheets_client.spreadsheets.return_value = self.spreadsheets
         mock_discovery.build.return_value = sheets_client
 
-    def get_create_data(self):
+    def get_create_data(self) -> Dict:
         """Get the data sent to create out of the mock."""
         return self.spreadsheets.create.call_args_list[0][1]["body"]
 
-    def test_sheet_title(self, mock_discovery) -> None:
+    def test_sheet_title(self, mock_discovery: mock.MagicMock) -> None:
         """Title contains the student name."""
         self.setup_mock_for_sheets(mock_discovery)
         student = self.StudentFactory.create()
